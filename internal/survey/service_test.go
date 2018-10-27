37c15fde53b95103a4762aa1118dc0f1327d56da
andygeiss <4ndygeiss@gmail.com>
2018-10-27 22:04:48 +0200

    path fixed

--- internal/survey/service_test.go
+++ internal/survey/service_test.go
@@ -10,23 +10,23 @@ import (
 )
 
 func TestDefaultService_FindSurveyByName_Should_Return_One_Survey(t *testing.T) {
-	expRepo := explanation.NewDefaultRepository("../../app_explanation.json")
-	infRepo := inference.NewDefaultRepository("../../app_inference.json")
-	engine := inference.NewDefaultEngine("Schadensklasse bestimmen", infRepo)
+	expRepo := explanation.NewDefaultRepository("../../testdata/explanation.json")
+	infRepo := inference.NewDefaultRepository("../../testdata/inference.json")
+	engine := inference.NewDefaultEngine("SURVEY NAME", infRepo)
 	service := survey.NewDefaultService(expRepo, engine)
-	qs, err := service.FindQuestionsBySurvey("Schadensklasse bestimmen")
+	qs, err := service.FindQuestionsBySurvey("SURVEY NAME")
 	assert.That(t, err, matchers.IsEqual(nil))
-	assert.That(t, len(qs), matchers.IsEqual(1))
-	assert.That(t, qs[0].Prompt, matchers.IsEqual("Auswirkungen?"))
+	assert.That(t, len(qs), matchers.IsEqual(2))
+	assert.That(t, qs[0].Prompt, matchers.IsEqual("Question 1 ?"))
 }
 
 func TestDefaultService_GetResultsByFacts_Should_Return_One_Fact(t *testing.T) {
-	expRepo := explanation.NewDefaultRepository("../../app_explanation.json")
-	infRepo := inference.NewDefaultRepository("../../app_inference.json")
-	engine := inference.NewDefaultEngine("Schadensklasse bestimmen", infRepo)
+	expRepo := explanation.NewDefaultRepository("../../testdata/explanation.json")
+	infRepo := inference.NewDefaultRepository("../../testdata/inference.json")
+	engine := inference.NewDefaultEngine("SURVEY NAME", infRepo)
 	service := survey.NewDefaultService(expRepo, engine)
 	facts, err := service.GetResultsByFacts([]string{"#Q1 = 1"})
 	assert.That(t, err, matchers.IsEqual(nil))
-	assert.That(t, len(facts), matchers.IsEqual(1))
-	assert.That(t, facts[0], matchers.IsEqual("keine nennenswerte Auswirkung auf die Geschäftstätigkeit"))
+	assert.That(t, len(facts), matchers.IsEqual(2))
+	assert.That(t, facts[0], matchers.IsEqual("Question 1 answered"))
 }
